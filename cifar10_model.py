f2792927df61265412996c4cd166372b5f510312
timohromadka <timo.hromadka@gmail.com>
2024-03-16 21:20:47 +0000

    Initialize with new out_channel parameter.

--- cifar10_model.py
+++ cifar10_model.py
@@ -6,16 +6,19 @@ BASE_NUM_STEPS = 1024
 BASE_TIME_SCALE = 1
 
 def make_model():
-    net = UNet(in_channel=3,  # Color images for CIFAR10
-               channel=128-16,
-               channel_multiplier=[1, 2, 2, 4, 4],
-               n_res_blocks=2,
-               attn_strides=[8, 16],
-               attn_heads=4,
-               use_affine_time=True,
-               dropout=0,
-               fold=1)
-    net.image_size = [3, 32, 32]  # Adjusted for CIFAR10's 32x32 images
+    net = UNet(
+        in_channel=3,  # Color images for CIFAR10
+        in_channel=3,
+        channel=128-16,
+        channel_multiplier=[1, 2, 2, 4, 4],
+        n_res_blocks=2,
+        attn_strides=[8, 16],
+        attn_heads=4,
+        use_affine_time=True,
+        dropout=0,
+        fold=1
+    )
+    net.image_size = [1, 3, 32, 32]  # Adjusted for CIFAR10's 32x32 images
     return net
 
 def make_dataset(train=True):
--- mnist_model.py
+++ mnist_model.py
@@ -6,16 +6,19 @@ BASE_NUM_STEPS = 1024
 BASE_TIME_SCALE = 1
 
 def make_model():
-    net = UNet(in_channel=1,  # Grayscale images for MNIST
-               channel=128-16,
-               channel_multiplier=[1, 2, 2, 4, 4],
-               n_res_blocks=2,
-               attn_strides=[8, 16],
-               attn_heads=4,
-               use_affine_time=True,
-               dropout=0,
-               fold=1)
-    net.image_size = [1, 1, 28, 28]  # Adjusted for MNIST's 28x28 images
+    net = UNet(
+        in_channel=1,  # Grayscale images for MNIST
+        out_channel=1,
+        channel=128-16,
+        channel_multiplier=[1, 2, 2, 4, 4],
+        n_res_blocks=2,
+        attn_strides=[8, 16],
+        attn_heads=4,
+        use_affine_time=True,
+        dropout=0,
+        fold=1
+        )
+    net.image_size = [1, 1, 32, 32]  # its still 32x32, as we resize them to this
     return net
 
 def make_dataset(train=True):
