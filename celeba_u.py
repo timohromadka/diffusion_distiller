67bbcd2db5244c28aea827426a8c9a19b298ea0a
timohromadka <timo.hromadka@gmail.com>
2024-03-16 21:22:31 +0000

    Add out_channel parameter.

--- celeba_u.py
+++ celeba_u.py
@@ -6,7 +6,9 @@ BASE_NUM_STEPS = 1024
 BASE_TIME_SCALE = 1
 
 def make_model():
-    net = UNet(in_channel = 3,
+    net = UNet(
+        in_channel = 3,
+        out_channel = 3,
         channel = 128-16,
         channel_multiplier = [1, 2, 2, 4, 4],
         n_res_blocks = 2,
@@ -14,7 +16,8 @@ def make_model():
         attn_heads = 4,
         use_affine_time = True,
         dropout = 0,
-        fold = 1)
+        fold = 1
+    )
     net.image_size = [1, 3, 256, 256]
     return net
 
