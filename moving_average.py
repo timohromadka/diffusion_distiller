7f6d84a5ebceb6e1460f5a89008e2c1037c0c9db
timohromadka <timo.hromadka@gmail.com>
2024-03-17 17:17:04 +0000

    Change default beta for ema.

--- moving_average.py
+++ moving_average.py
@@ -8,7 +8,7 @@ def init_ema_model(model, model_ema, device=None):
                 data = data.to(device)
             ep.data.copy_(data)
 
-def moving_average(model, model_ema, beta=0.999, device=None):
+def moving_average(model, model_ema, beta=0.9999, device=None):
     with torch.no_grad():
         for param, param_ema in zip(model.parameters(), model_ema.parameters()):
             data = param.data
